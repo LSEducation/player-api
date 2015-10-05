3e9df8de3190c720ea807fc0f8d38e87c7bceff1
Alexander Pasechnik <alexanderpa@interlink-ua.com>
2015-10-05 15:45:24 +0300

    Added getCallback check
    Avoid possible undefined target error.

--- javascript/froogaloop.js
+++ javascript/froogaloop.js
@@ -123,6 +123,10 @@ var Froogaloop = (function(){
             value: params
         });
 
+        if (!(/^https?:\/\/player.vimeo.com/).test(target.src)) {
+            return false;
+        }
+
         target.contentWindow.postMessage(data, playerOrigin);
     }
 
@@ -208,7 +212,7 @@ var Froogaloop = (function(){
      * Retrieves stored callbacks.
      */
     function getCallback(eventName, target_id) {
-        if (target_id) {
+        if (target_id && eventCallbacks[target_id]) {
             return eventCallbacks[target_id][eventName];
         }
         else {
